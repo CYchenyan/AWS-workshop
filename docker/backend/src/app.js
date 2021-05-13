46fb7f6ad3c0c41e19dd4d15da313dc58e6ee8c7
Yan  Chen <yan.chen@thoughtworks.com>
2021-05-13 22:15:27 +0800

    change file structure

---
+++ docker/backend/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const db = require('./db');
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'small noc car',
+    create: '2017.09.10',
+    color: 'grey'
+  })
+})
+
+app.get('/health-check',(req, res)=>{
+  res.send('I am health ^_^!')
+})
+
+app.get('/connect-db',(req, res, next) => {
+  db.raw('select VERSION() version')
+  .then(([rows, columns]) => rows[0])
+  .then((row) => res.json({ message: `Hello from MySQL ${row.version}` }))
+  .catch(next);
+})
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
